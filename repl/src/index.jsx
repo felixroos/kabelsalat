61fad19f1eff9171e25a8136609021c6fea24849
Felix Roos <flix91@gmail.com>
2024-06-14 12:17:10 +0200

    make default patch more interesting

--- repl/src/index.jsx
+++ repl/src/index.jsx
@@ -4,9 +4,36 @@ import "./graphviz";
 import "./index.css";
 import { SalatRepl } from "@kabelsalat/core";
 
-const tremoloSine = `sine(220)
-.mul(sine(4).range(.5,1))
-.out()`;
+const defaultPatch = `// let's create some notes
+let note = clock(150) // clock at 150bpm
+.clockdiv(32) // divide clock by 32
+.seq(64,0,0,67,0,0,62,0) // use clock for sequence
+
+// use the notes to trigger an envelope
+let env = note
+.adsr(
+  .01, // attack
+  sine(.1).range(.1, .3), // modulated decay
+  .5 // sustain
+)
+.mul(.75) // evelope amount
+
+note
+.hold(note) // hold notes above zeroes
+.midinote() // convert midi numbers to freq
+.pulse(.2) // pulse wave with .2 width
+.filter(
+  sine(.1).range(.7,.8).mul(env), // modulated cutoff
+  env.mul(.5) // resonance with envelope
+)
+.mul(env) // amplitude envelope
+.add(
+  // feedback delay
+  x=>x.delay(sine(.03).range(.1,.5)) // modulated delay time
+      .mul(.9) // feedback amount
+)
+.mul(.5) // master level
+.out() // send to output`;
 
 function App() {
   const repl = new SalatRepl();
@@ -16,7 +43,7 @@ function App() {
     urlCode = atob(urlCode);
     console.log("loaded code from url!");
   }
-  const initialCode = urlCode || tremoloSine;
+  const initialCode = urlCode || defaultPatch;
   let [code, setCode] = createSignal(initialCode);
   let container;
   async function run() {
