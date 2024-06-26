4c2e0c9aaec7d90a4a152d27611e5ee9fed73960
Felix Roos <flix91@gmail.com>
2024-06-26 15:57:54 +0200

    no deps..

--- packages/core/vite.config.js
+++ packages/core/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { dependencies } from "./package.json";
+// import { dependencies } from "./package.json";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
@@ -13,7 +13,7 @@ export default defineConfig({
       fileName: (ext) => ({ es: "index.mjs", iife: "index.js" }[ext]),
     },
     rollupOptions: {
-      external: [...Object.keys(dependencies)],
+      // external: [...Object.keys(dependencies)],
     },
     target: "esnext",
   },
