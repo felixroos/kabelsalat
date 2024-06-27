99b9af6707cca55a97e97349637399e0acc20851
Felix Roos <flix91@gmail.com>
2024-06-27 16:23:49 +0200

    add mdx

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()],
-  base: "kabelsalat/",
+  integrations: [tailwind(), solidJs(), mdx()],
+  base: "kabelsalat/"
 });
