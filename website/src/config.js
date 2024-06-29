98082fb0bd07c4192edfc72e7a0ae7950841a89d
Felix Roos <flix91@gmail.com>
2024-06-29 13:31:52 +0200

    pull out midi docs + add nav to docs

---
+++ website/src/config.js
@@ -0,0 +1,11 @@
+export const SIDEBAR = {
+  Links: [
+    { text: "REPL", link: "" },
+    { text: "Github", link: "https://github.com/felixroos/kabelsalat" },
+  ],
+  Tutorial: [
+    { text: "What is kabelsalat", link: "learn" },
+    { text: "MIDI", link: "midi" },
+  ],
+  Development: [{ text: "How kabelsalat works", link: "internals" }],
+};
