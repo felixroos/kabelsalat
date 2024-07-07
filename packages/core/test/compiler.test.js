2819067041eafaf482cbf9525d715eaf28c5d947
Felix Roos <flix91@gmail.com>
2024-07-07 03:14:24 +0200

    test feedback compilation

--- packages/core/test/compiler.test.js
+++ packages/core/test/compiler.test.js
@@ -1,21 +1,31 @@
-import { node } from "../src/index";
+import "../src/compiler";
 import * as api from "../src/lib";
 import { describe, expect, it } from "vitest";
 Object.assign(globalThis, api);
 
 describe("compiler", () => {
-  it("empty", () => {
-    expect(node("exit").compile().src).toStrictEqual(
-      "return [(0*lvl), (0*lvl)]"
-    );
-  });
   it("sine", () => {
     const unit = sine(200).out().exit().compile();
     expect(unit.src).toStrictEqual(
       `const n2 = nodes[0].update(200, 0, 0); /* sine */
 return [(n2*lvl), (n2*lvl)]`
     );
-    console.log("unit", unit);
     expect(unit.ugens).toStrictEqual(["SineOsc"]);
+  });
+  it("feedback", () => {
+    const unit = sine(200)
+      .add((x) => x.mul(0.8))
+      .out()
+      .exit()
+      .compile();
+    expect(unit.src).toStrictEqual(
+      `const n5 = nodes[0].update(); /* feedback_read */
+const n3 = nodes[1].update(200, 0, 0); /* sine */
+const n2 = n3 + n5; /* sine + feedback_read */
+const n7 = n2 * 0.8; /* add * n */
+const n6 = nodes[0].write(n7); // feedback_write
+return [(n2*lvl), (n2*lvl)]`
+    );
+    expect(unit.ugens).toStrictEqual(["Feedback", "SineOsc"]);
   });
 });
