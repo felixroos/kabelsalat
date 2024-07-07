a58c621b1aa9dea9447678acc871eaf1f52ef7a5
Felix Roos <flix91@gmail.com>
2024-07-07 05:49:57 +0200

    fix: test

--- packages/core/test/compiler.test.js
+++ packages/core/test/compiler.test.js
@@ -7,7 +7,7 @@ describe("compiler", () => {
   it("sine", () => {
     const unit = sine(200).out().exit().compile();
     expect(unit.src).toStrictEqual(
-      `const n2 = nodes[0].update(200, 0, 0); /* sine */
+      `const n2 = nodes[0].update(200,0,0); /* sine */
 return [(n2*lvl), (n2*lvl)]`
     );
     expect(unit.ugens).toStrictEqual(["SineOsc"]);
@@ -20,7 +20,7 @@ return [(n2*lvl), (n2*lvl)]`
       .compile();
     expect(unit.src).toStrictEqual(
       `const n5 = nodes[0].update(); /* feedback_read */
-const n3 = nodes[1].update(200, 0, 0); /* sine */
+const n3 = nodes[1].update(200,0,0); /* sine */
 const n2 = n3 + n5; /* add */
 const n7 = n2 * 0.8; /* mul */
 const n6 = nodes[0].write(n7); /* feedback_write */
